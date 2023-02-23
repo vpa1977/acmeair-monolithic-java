f2760f14e0b99ed8fbaf14bb29b3e1af18c067c2
vpa1977 <vladimir.petko@canonical.com>
2023-02-24 10:00:07 +1300

    remove mongo driver autoconfig (fixes startup npe)

--- src/main/java/com/acmeair/web/AcmeAirApp.java
+++ src/main/java/com/acmeair/web/AcmeAirApp.java
@@ -2,12 +2,15 @@ package com.acmeair.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SpringBootConfiguration
 @ComponentScan(basePackages = {"com.acmeair"})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class AcmeAirApp {
    	public static void main(String[] args) {
 		SpringApplication.run(AcmeAirApp.class, args);
