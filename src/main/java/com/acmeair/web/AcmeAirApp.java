6973aa1c60f42c59b2d3a98825391ab5326a97a2
vpa1977 <vladimir.petko@canonical.com>
2023-02-24 10:56:37 +1300

    move servlet initializer into app

--- src/main/java/com/acmeair/web/AcmeAirApp.java
+++ src/main/java/com/acmeair/web/AcmeAirApp.java
@@ -5,15 +5,21 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SpringBootConfiguration
 @ComponentScan(basePackages = {"com.acmeair"})
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
-public class AcmeAirApp {
-   	public static void main(String[] args) {
+public class AcmeAirApp extends SpringBootServletInitializer {
+	public static void main(String[] args) {
 		SpringApplication.run(AcmeAirApp.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AcmeAirApp.class);
+	}
 }
